1cdcdf2620125cd34660b571f4f957c751da157b
devgansahil111 <devgansahil111@gmail.com>
2022-02-22 21:18:51 +0530

    task done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,50 +1,58 @@
 const express = require('express');
+const underscore = require('underscore')
 const router = express.Router();
+const logger = require('../logger/logger')
+const helper = require('../util/helper')
+const formatter = require('../validator/formatter')
+const lodash = require('lodash')
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
 
-// 1.
-router.get('/movies/:name', function (req,res){
-    // let movies = ["The Shining","Incendies","Rang De Basanti","Finding Demo"]
-    let moviesName = req.params.name
-    console.log(moviesName)
-    res.send(moviesName)
-})
+router.get('/test-me', function (req, res) {
+    let names = [ 'Hariom', 'Arpit', 'Akash', 'Sabiha']
+    //Module 1 members
+    logger.printMessage('thorium')
+    console.log(logger.url)
+    logger.printWelcomeMessage()
 
-// 2. and 3.
-router.get('/movies/:movieId',function(req,res){
-    mov=["Fukrey","Delhi","Dabang","Rockstar","Suryavanshi"]
-    let value = req.params.movieId;
-    if (value>mov.length-1){
-        res.send("not exist")
-    } else {
-        res.send(mov[value])
-    }
+    //Module 2 functions
+    helper.printCurrentDate()
+    helper.printCurrentMonth()
+    helper.printBatchInfo()
+
+    //Module 3 functions
+    formatter.trim()
+    formatter.changeToUpperCase()
+    formatter.changetoLowerCase()
+
+    //Lodash functions
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    let subArrays = lodash.chunk(months, 3)
+    console.log('Chunks of months: ', subArrays)
+    
+    let oddNumbers = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+    let lastNumbers = lodash.tail(oddNumbers)
+    console.log('Last 9 odd numbers: ',lastNumbers)
+
+    let arr1 = [1, 2, 3]
+    let arr2 = [2, 3, 4, 4]
+    let arr3 = [4, 5]
+    let arr4 = [4, 6, 4]
+    let arr5 = [5, 8]
+    console.log('Merged array with unique values: ',lodash.union(arr1, arr2, arr3, arr4, arr5))
+
+    let movie1 = ['horror', 'The Shining']
+    let movie2 = ['drama','Titanic']
+    let movie3 = ['thriller','Shutter Island']
+    let movie4 = ['fantasy','Pans Labyrinth']
+    let movieObject = lodash.fromPairs([movie1, movie2, movie3, movie4])
+    console.log('Movies object: ', movieObject)
+    res.send('My first ever api of the week!')
 });
 
-// 4.
-router.get('/moviez',function(req,res){
-    res.send([{id:1,name:"The Shining"},{id:2,name:"Incendies"},{id:3,name:"Rang De Basanti"},{id:4,name:"Finding Demo"}])
+
+router.get('/hello', function (req, res) {
+    
+    res.send('My first ever api of the week!')
 });
-
-// 5.
-router.get('/films/:filmId',function(req,res){
-    res.send([{id:1,name:"The Shining"},{id:2,name:"Incendies"},{id:3,name:"Rang De Basanti"},{id:4,name:"Finding Demo"}])
-    let value = req.params.filmId;
-    let found=false;
-    for (i=0;i<movi.length;i++){
-        if (movi[i].id==value){
-            found=true
-            res.send(movi[i])
-            break
-        }
-    }
-});
-
-
 
 module.exports = router;
