79bda3224f5dcb2900d063b00d270f42e5565629
devgansahil111 <devgansahil111@gmail.com>
2022-02-22 20:32:55 +0530

    added

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,43 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
+// 1.
+router.get('/movies/:name', function (req,res){
+    // let movies = ["The Shining","Incendies","Rang De Basanti","Finding Demo"]
+    let moviesName = req.params.name
+    console.log(moviesName)
+    res.send(moviesName)
+})
+
+// 2. and 3.
+router.get('/movies/:movieId',function(req,res){
+    mov=["Fukrey","Delhi","Dabang","Rockstar","Suryavanshi"]
+    let value = req.params.movieId;
+    if (value>mov.length-1){
+        res.send("not exist")
+    } else {
+        res.send(mov[value])
+    }
+});
+
+// 4.
+router.get('/moviez',function(req,res){
+    res.send([{id:1,name:"The Shining"},{id:2,name:"Incendies"},{id:3,name:"Rang De Basanti"},{id:4,name:"Finding Demo"}])
+});
+
+// 5.
+router.get('/films/:filmId',function(req,res){
+    res.send([{id:1,name:"The Shining"},{id:2,name:"Incendies"},{id:3,name:"Rang De Basanti"},{id:4,name:"Finding Demo"}])
+    let value = req.params.filmId;
+    let found=false;
+    for (i=0;i<movi.length;i++){
+        if (movi[i].id==value){
+            found=true
+            res.send(movi[i])
+            break
+        }
+    }
+});
 
 
 
